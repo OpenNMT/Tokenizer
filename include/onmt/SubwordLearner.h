8d5e25887fa435c6b2839bffb274e78750065522
Guillaume Klein <guillaume.klein@systrangroup.com>
2019-05-23 18:42:27 +0200

    Fix SubwordLearner interface

--- include/onmt/SubwordLearner.h
+++ include/onmt/SubwordLearner.h
@@ -16,8 +16,8 @@ namespace onmt
   public:
     SubwordLearner(bool verbose);
     virtual ~SubwordLearner() = default;
-    virtual void ingest(std::istream& in, const Tokenizer* tokenizer) = 0;
-    virtual void learn(std::ostream& out, const char* description) = 0;
+    virtual void ingest(std::istream& in, const Tokenizer* tokenizer = nullptr) = 0;
+    virtual void learn(std::ostream& out, const char* description = nullptr) = 0;
   protected:
     bool _verbose;
     std::unique_ptr<const Tokenizer> _default_tokenizer;
