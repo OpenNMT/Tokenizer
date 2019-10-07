2162a57f7aadf0a24f7362717a400b3feb93cda3
Guillaume Klein <guillaume.klein@systrangroup.com>
2019-10-07 17:36:22 +0200

    Bump version

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -35,7 +35,7 @@ tokenizer_module = Extension(
 
 setup(
     name="pyonmttok",
-    version="1.15.7",
+    version="1.16.0",
     license="MIT",
     description="OpenNMT tokenization library",
     author="OpenNMT",
