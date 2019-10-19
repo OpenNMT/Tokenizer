473fdb3174cd4ecd33b484034aeff258145512e7
Guillaume Klein <guillaume.klein@systrangroup.com>
2019-10-19 09:27:52 +0200

    Always ignore placeholders when learning subword models
    When using SentencePieceLearner without a tokenizer, the input text
    was just forwarded to an internal training file. However, for good
    integration in the OpenNMT tokenizer, we always need to ignore
    placeholders that could be present in the input text.

--- src/SubwordLearner.cc
+++ src/SubwordLearner.cc
@@ -7,15 +7,33 @@
 namespace onmt
 {
 
-  SubwordLearner::SubwordLearner(bool verbose)
+  SubwordLearner::SubwordLearner(bool verbose, const Tokenizer* default_tokenizer)
     : _verbose(verbose)
+    , _default_tokenizer(default_tokenizer
+                         ? default_tokenizer
+                         : new Tokenizer(Tokenizer::Mode::None, Tokenizer::Flags::NoSubstitution))
   {
   }
 
   void SubwordLearner::ingest(const std::string& text, const Tokenizer* tokenizer)
   {
-    std::istringstream in(text);
-    ingest(in, tokenizer);
+    if (!tokenizer)
+      tokenizer = _default_tokenizer.get();
+
+    std::vector<AnnotatedToken> tokens;
+    tokenizer->tokenize(text, tokens);
+    for (const auto& token : tokens)
+    {
+      if (!Tokenizer::is_placeholder(token.str()))
+        ingest_token(token.str());
+    }
+  }
+
+  void SubwordLearner::ingest(std::istream& is, const Tokenizer* tokenizer)
+  {
+    std::string line;
+    while (std::getline(is, line))
+      ingest(line, tokenizer);
   }
 
   void SubwordLearner::learn(const std::string& model_path, const char* description, bool verbose)
