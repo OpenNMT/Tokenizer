185a64bd8f372e564a6cf49ce8f58a8153d29392
Guillaume Klein <guillaumekln@users.noreply.github.com>
2021-02-16 15:24:32 +0100

    Use -O3 optimization flag when compiling ICU for the Python wheels (#228)

--- bindings/python/tools/prepare_build_environment.sh
+++ bindings/python/tools/prepare_build_environment.sh
@@ -10,7 +10,7 @@ ICU_VERSION=${ICU_VERSION:-64.2}
 curl -L -O https://github.com/unicode-org/icu/releases/download/release-${ICU_VERSION/./-}/icu4c-${ICU_VERSION/./_}-src.tgz
 tar xf icu4c-*-src.tgz
 cd icu/source
-CFLAGS="-fPIC" CXXFLAGS="-fPIC" ./configure --disable-shared --enable-static
+CFLAGS="-O3 -fPIC" CXXFLAGS="-O3 -fPIC" ./configure --disable-shared --enable-static
 make -j2 install
 cd $ROOT_DIR
 
