444e4565281ff93e5c6105c36024b03dc70cde7a
Guillaume Klein <guillaume.klein@systrangroup.com>
2021-02-16 14:48:53 +0100

    Update ICU to a newer version in Python wheels
    More recent ICU versions are available but the GCC version in
    manylinux1 is too old to compile them.

--- bindings/python/tools/prepare_build_environment.sh
+++ bindings/python/tools/prepare_build_environment.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 ROOT_DIR=$PWD
-ICU_VERSION=${ICU_VERSION:-64.2}
+ICU_VERSION=${ICU_VERSION:-66.1}
 
 # Install ICU.
 curl -L -O https://github.com/unicode-org/icu/releases/download/release-${ICU_VERSION/./-}/icu4c-${ICU_VERSION/./_}-src.tgz
