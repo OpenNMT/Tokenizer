93a41a2d492c519533de36fd9e372f40ab911f93
Guillaume Klein <guillaumekln@users.noreply.github.com>
2021-09-15 09:55:41 +0200

    Build Python wheels for Windows (#260)
    * Build Python wheels for Windows
    
    * Update Python README
    
    * Pin OS version when building wheels

--- bindings/python/pyonmttok/__init__.py
+++ bindings/python/pyonmttok/__init__.py
@@ -1,1 +1,19 @@
+import sys
+
+if sys.platform == "win32":
+    import os
+    import ctypes
+    import pkg_resources
+
+    module_name = sys.modules[__name__].__name__
+    package_dir = pkg_resources.resource_filename(module_name, "")
+
+    add_dll_directory = getattr(os, "add_dll_directory", None)
+    if add_dll_directory is not None:
+        add_dll_directory(package_dir)
+
+    for filename in os.listdir(package_dir):
+        if filename.endswith(".dll"):
+            ctypes.CDLL(os.path.join(package_dir, filename))
+
 from ._ext import *
