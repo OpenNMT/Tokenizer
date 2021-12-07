6046976e4d8f6ea995655751885946617d8cb546
Guillaume Klein <guillaume.klein@systrangroup.com>
2021-12-07 17:31:07 +0100

    Revert "Cache compiled ICU library in CI"
    This reverts commit e3e62d980ecb9112849ed147e2bf41a03d3b4277.

--- bindings/python/tools/prepare_build_environment.sh
+++ bindings/python/tools/prepare_build_environment.sh
@@ -7,14 +7,12 @@ ROOT_DIR=$PWD
 ICU_VERSION=${ICU_VERSION:-70.1}
 
 # Install ICU.
-if [ ! -d "$ROOT_DIR/libicu" ]; then
-    curl -L -O https://github.com/unicode-org/icu/releases/download/release-${ICU_VERSION/./-}/icu4c-${ICU_VERSION/./_}-src.tgz
-    tar xf icu4c-*-src.tgz
-    cd icu/source
-    CFLAGS="-O3 -fPIC" CXXFLAGS="-O3 -fPIC" ./configure --disable-shared --enable-static --prefix=$ROOT_DIR/libicu
-    make -j2 install
-    cd $ROOT_DIR
-fi
+curl -L -O https://github.com/unicode-org/icu/releases/download/release-${ICU_VERSION/./-}/icu4c-${ICU_VERSION/./_}-src.tgz
+tar xf icu4c-*-src.tgz
+cd icu/source
+CFLAGS="-O3 -fPIC" CXXFLAGS="-O3 -fPIC" ./configure --disable-shared --enable-static
+make -j2 install
+cd $ROOT_DIR
 
 # Install cmake.
 pip install "cmake==3.18.*"
@@ -23,6 +21,6 @@ pip install "cmake==3.18.*"
 rm -rf build
 mkdir build
 cd build
-cmake -DLIB_ONLY=ON -DCMAKE_PREFIX_PATH=$ROOT_DIR/libicu ..
+cmake -DLIB_ONLY=ON ..
 make -j2 install
 cd $ROOT_DIR
