9ab5de2188f6a6efc088e481c592b6f7cf743ed4
vince62s <vince62s@yahoo.com>
2025-12-30 11:03:35 +0100

    .

--- bindings/python/tools/prepare_build_environment_linux.sh
+++ bindings/python/tools/prepare_build_environment_linux.sh
@@ -1,41 +1,29 @@
-#!/usr/bin/env bash
-set -euxo pipefail
+#! /bin/bash
 
-# Use /project as the root since that's where cibuildwheel mounts the repo
-ROOT_DIR="/project"
-ICU_ROOT="$ROOT_DIR/icu"
-INSTALL_DIR="$ROOT_DIR/build/install"
+set -e
+set -x
 
-# manylinux compiler flags (required)
-export CFLAGS="-O3 -fPIC"
-export CXXFLAGS="-O3 -fPIC"
+ROOT_DIR=$PWD
+ICU_ROOT=$ROOT_DIR/icu
+CMAKE_EXTRA_ARGS=""
 
-# Build ICU from source
+# Download and compile ICU from sources.
 ICU_VERSION=${ICU_VERSION:-73.2}
-ICU_TGZ="icu4c-${ICU_VERSION/./_}-src.tgz"
-curl -LO "https://github.com/unicode-org/icu/releases/download/release-${ICU_VERSION/./-}/${ICU_TGZ}"
-tar xf "$ICU_TGZ"
-pushd icu/source
-./configure \
-  --disable-shared \
-  --enable-static \
-  --prefix="$ICU_ROOT"
-make -j$(nproc)
-make install
-popd
+curl -L -O https://github.com/unicode-org/icu/releases/download/release-${ICU_VERSION/./-}/icu4c-${ICU_VERSION/./_}-src.tgz
+tar xf icu4c-*-src.tgz
+cd icu/source
+CFLAGS="-O3 -fPIC" CXXFLAGS="-O3 -fPIC" ./configure --disable-shared --enable-static --prefix=$ICU_ROOT
+make -j2 install
 
-# Build Tokenizer C++ library as static
-rm -rf "$ROOT_DIR/build"
-mkdir -p "$ROOT_DIR/build"
-pushd "$ROOT_DIR/build"
-cmake \
-  -DLIB_ONLY=ON \
-  -DBUILD_SHARED_LIBS=OFF \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-  -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
-  -DICU_ROOT="$ICU_ROOT" \
-  "$ROOT_DIR"
-make -j$(nproc)
-make install
-popd
+cd $ROOT_DIR
+
+# Install cmake.
+pip install cmake
+
+# Build Tokenizer.
+rm -rf build
+mkdir build
+cd build
+cmake -DLIB_ONLY=ON -DICU_ROOT=$ICU_ROOT $CMAKE_EXTRA_ARGS ..
+VERBOSE=1 make -j2 install
+cd $ROOT_DIR
