903968b0009513654d1acdea8cd2a67a49e9c963
vince62s <vince62s@yahoo.com>
2025-12-30 10:02:57 +0100

    another try

--- bindings/python/tools/prepare_build_environment_linux.sh
+++ bindings/python/tools/prepare_build_environment_linux.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 
 ROOT_DIR="$PWD"
 ICU_ROOT="$ROOT_DIR/icu"
+INSTALL_DIR="${ROOT_DIR}/build/install"
 
 # manylinux compiler flags (required)
 export CFLAGS="-O3 -fPIC"
@@ -11,10 +12,8 @@ export CXXFLAGS="-O3 -fPIC"
 # Build ICU from source
 ICU_VERSION=${ICU_VERSION:-73.2}
 ICU_TGZ="icu4c-${ICU_VERSION/./_}-src.tgz"
-
 curl -LO "https://github.com/unicode-org/icu/releases/download/release-${ICU_VERSION/./-}/${ICU_TGZ}"
 tar xf "$ICU_TGZ"
-
 pushd icu/source
 ./configure \
   --disable-shared \
@@ -26,18 +25,15 @@ popd
 
 # Build Tokenizer C++ library
 rm -rf build
-mkdir build
+mkdir -p build
 pushd build
-
 cmake \
   -DLIB_ONLY=ON \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+  -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
   -DICU_ROOT="$ICU_ROOT" \
   ..
-
 make -j$(nproc)
-make install DESTDIR="$ROOT_DIR/build"
-
+make install
 popd
-
